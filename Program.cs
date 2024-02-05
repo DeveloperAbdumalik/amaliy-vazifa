51d716d9d231d5a0d05c539dbbc53e41df7d07ed
DeveloperAbdumalik <dreamyoutobe1@gmail.com>
2024-02-06 01:49:56 +0300

    INFRA: Upload project

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿/*System.Console.WriteLine("Doira yuzi va aylana uzunligini hisoblash:");
+System.Console.Write("Radiusni kiriting r=");
+double  r = Convert.ToDouble(Console.ReadLine());
+double s,l,pi=3.14d;
+s=pi*r*r;
+l=2*pi*r; 
+System.Console.WriteLine($"Doira yuzi S = {s},\nAylana uzunligi {l}");*/
+
+
+/*System.Console.WriteLine("Valyuta konvertri:");
+System.Console.Write("Qiymat summani kiriting q = ");
+double q = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("SUM dagi valyuta kursini kiriting sum = ");
+double sum = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine($"Sumga kanvertatsiya qilingan qiymat: {q*sum}");*/
+
+
+System.Console.WriteLine("Yoshni hisoblash:");
+System.Console.WriteLine("Foydalanuvchining tug'ilgan kunini kiriting: ");
+int age = Convert.ToInt32(Console.ReadLine());
+int year = 2023;
+System.Console.WriteLine($"Foydalanuvchi {(year-age)*365} kun yashagan.");
