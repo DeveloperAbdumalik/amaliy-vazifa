f9ef2bb5e04fb1d03cb0a390dc7187e04c74cb06
DeveloperAbdumalik <dreamyoutobe1@gmail.com>
2024-02-06 14:36:14 +0300

    2-amaliy vazifa

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ l=2*pi*r;
 System.Console.WriteLine($"Doira yuzi S = {s},\nAylana uzunligi {l}");*/
 
 
+
 /*System.Console.WriteLine("Valyuta konvertri:");
 System.Console.Write("Qiymat summani kiriting q = ");
 double q = Convert.ToDouble(Console.ReadLine());
@@ -15,8 +16,69 @@ double sum = Convert.ToDouble(Console.ReadLine());
 System.Console.WriteLine($"Sumga kanvertatsiya qilingan qiymat: {q*sum}");*/
 
 
-System.Console.WriteLine("Yoshni hisoblash:");
+
+/*System.Console.WriteLine("Yoshni hisoblash:");
 System.Console.WriteLine("Foydalanuvchining tug'ilgan kunini kiriting: ");
 int age = Convert.ToInt32(Console.ReadLine());
 int year = 2023;
-System.Console.WriteLine($"Foydalanuvchi {(year-age)*365} kun yashagan.");
+System.Console.WriteLine($"Foydalanuvchi {(year-age)*365} kun yashagan.");*/
+
+
+
+/*System.Console.WriteLine("String ustida amallar: ");
+System.Console.Write("Satir kiriting: ");
+string str = Console.ReadLine();
+System.Console.WriteLine("Butun son kiriting: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int a = Convert.ToInt32(str.Length);
+
+string result = (x>=a)
+    ? str.ToUpper()
+    : str.ToLower();
+System.Console.WriteLine(result);**/
+
+
+
+/*int x = 15;
+int y = 10;
+if(x > y)
+{
+    System.Console.WriteLine("x is greater than y");
+}
+else if(x < y)
+{
+    System.Console.WriteLine("x is less then y");
+}
+else if(x==y)
+{
+    System.Console.WriteLine("x is equal to y");
+}
+else
+    System.Console.WriteLine("x and y are not comparable");*/
+
+
+
+System.Console.WriteLine("Hafta kunlari:");
+System.Console.Write("Hafta kunini kiriting: ");
+string str = Console.ReadLine();
+string day = str.ToLower();
+switch(day)
+{
+    case "dushanba": System.Console.WriteLine("monday");
+        break;
+    case "seshanba": System.Console.WriteLine("tuesday");
+        break;
+    case "chorshanba": System.Console.WriteLine("wednesday");
+        break;
+    case "payshanba": System.Console.WriteLine("thursday");
+        break;
+    case "juma": System.Console.WriteLine("friday");
+        break;
+    case "shanba": System.Console.WriteLine("saturday");
+        break;
+    case "yakshanba": System.Console.WriteLine("sunday");
+        break;
+    default:
+        System.Console.WriteLine("Bunaqa hafta kuni yuq:");
+        break;
+}
